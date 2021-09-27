3dc5f1f8a46bb9b20421650f1dedcb1b2a03bf92
Alexis Berthelot <berthelota2@gmail.com>
2021-09-27 14:10:37 +0200

    assessment controller

---
+++ src/main/java/com/mediscreen/assessment/controller/AssessmentController.java
@@ -0,0 +1,48 @@
+package com.mediscreen.assessment.controller;
+
+import com.mediscreen.assessment.model.DiabeteRiskLevel;
+import com.mediscreen.assessment.model.Patient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@RestController
+public class AssessmentController {
+
+
+    @RequestMapping("/assess/id")
+    public String accessPatient(@RequestParam(value="patId") Long patId) {
+        return generateAssessment(getPatientDemographics(patId));
+    }
+
+    @RequestMapping("/assess/familyName")
+    public String accessPatient(@RequestParam(value="familyName") String familyName) {
+        return generateAssessment(getPatientDemographics(familyName));
+    }
+
+    private Patient getPatientDemographics(Long patId) {
+        final String demographicsUri = "http://localhost:8081/patient/getById?Id=" + patId;
+        return new RestTemplate().getForObject(demographicsUri, Patient.class);
+    }
+
+    private Patient getPatientDemographics(String familyName) {
+        final String demographicsUri = "http://localhost:8081/patient/getByFamilyName?Name=" + familyName;
+        return new RestTemplate().getForObject(demographicsUri, Patient.class);
+    }
+
+    private String generateAssessment (Patient patient) {
+        return new DiabeteRiskLevel().access(patient, getPatientHistory(patient.getId()));
+    }
+
+    private List<LinkedHashMap<String,String>> getPatientHistory(Long patId) {
+        final String historyUri = "http://localhost:8082/patHistory/get?patId=" + patId;
+        return (List<LinkedHashMap<String,String>>) new RestTemplate().getForObject(historyUri, List.class);
+    }
+
+
+
+}
